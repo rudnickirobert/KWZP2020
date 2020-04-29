f95ef8105325d9a171973bbea134739f33a1bc14
Lukasz Zarzycki <zrider100z@wp.pl>
2020-04-29 10:07:35 +0200

    Forms ready to use.

--- Szwalnia/Element_szczegoly.cs
+++ Szwalnia/Element_szczegoly.cs
@@ -15,7 +15,7 @@ namespace Szwalnia
         public Element_szczegoly(SzwalniaEntities db, int ID)
         {
             InitializeComponent();
-            Elementy element = db.Elementy.Where(wybrany => wybrany.Element_Nazwa == "Karton klapowy").First();
+            Elementy element = db.Elementy.Where(wybrany => wybrany.ID_Element == ID).First();
             lblElement.Text = "Szczegoly elementu o ID: " + element.ID_Element;
             txtNazwa.Text = element.Element_Nazwa;
             if (element.Okres_Przydatnosci_Miesiace == 0)
@@ -28,11 +28,9 @@ namespace Szwalnia
             }    
             txtIlosc.Text = element.Element_Ilosc_W_Paczce.ToString();
             txtJednostka.Text = db.Elementy_Jednostki.Where(unit => unit.ID_jednostka == element.ID_Jednostka).First().Jednostka;
-
-            //vCechyElementu vcecha = db.vCechyElementu.Where(details => details.ID == element.ID_Element).First();
-            //label1.Text = vcecha.Jednostka;
-            //lblElement.Text = db.vCechyElementu.Where(pick => pick.ID == element.ID_Element).First().Opis_slowny;
-            dgvSzczegol.DataSource = db.vCechyElementu.Where(details => details.ID == element.ID_Element).First();
+            //txtJednostka.Text = db.vCechyElementu.Where(details => details.ID == element.ID_Element).First().Jednostka;
+            //lblElement.Text = db.vCechyElementu.Where(pick => pick.ID == element.ID_Element).First().Jednostka;
+            dgvSzczegol.DataSource = db.vCechyElementu.Where(details => details.ID == element.ID_Element).ToList();
              
         }
 
--- Szwalnia/sysdiagrams.cs
+++ Szwalnia/sysdiagrams.cs
@@ -12,11 +12,12 @@ namespace Szwalnia
     using System;
     using System.Collections.Generic;
     
-    public partial class vCechyElementu
+    public partial class sysdiagrams
     {
-        public int ID { get; set; }
-        public string Cecha { get; set; }
-        public Nullable<float> Wartosc_liczbowa { get; set; }
-        public string Jednostka { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
