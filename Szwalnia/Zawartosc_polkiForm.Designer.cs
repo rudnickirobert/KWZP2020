90c5c73a392ac3c7ed516312ce8be0915ae514fe
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-04 15:16:32 +0200

    Zaprojektowany formularz Zawartosc_polkiForm

--- Szwalnia/Zawartosc_polkiForm.Designer.cs
+++ Szwalnia/Zawartosc_polkiForm.Designer.cs
@@ -28,12 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblNazwaElementu = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblIlePaczek = new System.Windows.Forms.Label();
+            this.txtIlePaczek = new System.Windows.Forms.TextBox();
+            this.lbIPaczka = new System.Windows.Forms.Label();
+            this.txtPaczka = new System.Windows.Forms.TextBox();
+            this.lblJednostka = new System.Windows.Forms.Label();
+            this.lblJednostka2 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLacznie = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(122, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(211, 36);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Regal + polka";
+            // 
+            // lblNazwaElementu
+            // 
+            this.lblNazwaElementu.AutoSize = true;
+            this.lblNazwaElementu.Location = new System.Drawing.Point(59, 133);
+            this.lblNazwaElementu.Name = "lblNazwaElementu";
+            this.lblNazwaElementu.Size = new System.Drawing.Size(86, 13);
+            this.lblNazwaElementu.TabIndex = 5;
+            this.lblNazwaElementu.Text = "Nazwa elementu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lblIlePaczek
+            // 
+            this.lblIlePaczek.AutoSize = true;
+            this.lblIlePaczek.Location = new System.Drawing.Point(59, 173);
+            this.lblIlePaczek.Name = "lblIlePaczek";
+            this.lblIlePaczek.Size = new System.Drawing.Size(67, 13);
+            this.lblIlePaczek.TabIndex = 7;
+            this.lblIlePaczek.Text = "Ilość paczek";
+            // 
+            // txtIlePaczek
+            // 
+            this.txtIlePaczek.Location = new System.Drawing.Point(186, 170);
+            this.txtIlePaczek.Name = "txtIlePaczek";
+            this.txtIlePaczek.Size = new System.Drawing.Size(197, 20);
+            this.txtIlePaczek.TabIndex = 6;
+            // 
+            // lbIPaczka
+            // 
+            this.lbIPaczka.AutoSize = true;
+            this.lbIPaczka.Location = new System.Drawing.Point(59, 215);
+            this.lbIPaczka.Name = "lbIPaczka";
+            this.lbIPaczka.Size = new System.Drawing.Size(67, 13);
+            this.lbIPaczka.TabIndex = 9;
+            this.lbIPaczka.Text = "Ile w paczce";
+            // 
+            // txtPaczka
+            // 
+            this.txtPaczka.Location = new System.Drawing.Point(186, 212);
+            this.txtPaczka.Name = "txtPaczka";
+            this.txtPaczka.Size = new System.Drawing.Size(197, 20);
+            this.txtPaczka.TabIndex = 8;
+            // 
+            // lblJednostka
+            // 
+            this.lblJednostka.AutoSize = true;
+            this.lblJednostka.Location = new System.Drawing.Point(389, 215);
+            this.lblJednostka.Name = "lblJednostka";
+            this.lblJednostka.Size = new System.Drawing.Size(53, 13);
+            this.lblJednostka.TabIndex = 10;
+            this.lblJednostka.Text = "jednostka";
+            // 
+            // lblJednostka2
+            // 
+            this.lblJednostka2.AutoSize = true;
+            this.lblJednostka2.Location = new System.Drawing.Point(389, 255);
+            this.lblJednostka2.Name = "lblJednostka2";
+            this.lblJednostka2.Size = new System.Drawing.Size(53, 13);
+            this.lblJednostka2.TabIndex = 13;
+            this.lblJednostka2.Text = "jednostka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Łącznie";
+            // 
+            // txtLacznie
+            // 
+            this.txtLacznie.Location = new System.Drawing.Point(186, 252);
+            this.txtLacznie.Name = "txtLacznie";
+            this.txtLacznie.Size = new System.Drawing.Size(197, 20);
+            this.txtLacznie.TabIndex = 11;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(59, 96);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(64, 13);
+            this.lblID.TabIndex = 15;
+            this.lblID.Text = "ID elementu";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(186, 93);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(197, 20);
+            this.txtID.TabIndex = 14;
+            // 
+            // Zawartosc_polkiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Zawartosc_polki";
+            this.ClientSize = new System.Drawing.Size(475, 311);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblJednostka2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLacznie);
+            this.Controls.Add(this.lblJednostka);
+            this.Controls.Add(this.lbIPaczka);
+            this.Controls.Add(this.txtPaczka);
+            this.Controls.Add(this.lblIlePaczek);
+            this.Controls.Add(this.txtIlePaczek);
+            this.Controls.Add(this.lblNazwaElementu);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Zawartosc_polkiForm";
+            this.Text = "Zawartość półki";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblNazwaElementu;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblIlePaczek;
+        private System.Windows.Forms.TextBox txtIlePaczek;
+        private System.Windows.Forms.Label lbIPaczka;
+        private System.Windows.Forms.TextBox txtPaczka;
+        private System.Windows.Forms.Label lblJednostka;
+        private System.Windows.Forms.Label lblJednostka2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLacznie;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
     }
 }
