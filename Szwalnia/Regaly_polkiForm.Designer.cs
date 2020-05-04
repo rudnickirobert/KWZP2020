7709b05724dbdc39991235751e499f57a436efdb
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-04 02:47:57 +0200

    Otwiera się Zapelnienie_regalu

--- Szwalnia/Regaly_polkiForm.Designer.cs
+++ Szwalnia/Regaly_polkiForm.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Regaly_polkiForm";
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(115, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 36);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "WYSZUKAJ REGAŁ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Oznaczenie regału";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(203, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "SZUKAJ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Regaly_polkiForm
+            // 
+            this.ClientSize = new System.Drawing.Size(511, 256);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Regaly_polkiForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Szwalnia/Regaly_polkiForm.cs
+++ Szwalnia/Regaly_polkiForm.cs
@@ -12,9 +12,32 @@ namespace Szwalnia
 {
     public partial class Regaly_polkiForm : Form
     {
-        public Regaly_polkiForm(SzwalniaEntities szwalnia_passed)
+        public SzwalniaEntities szwalnia_passed;
+        public Regaly_polkiForm(SzwalniaEntities szwalnia)
         {
             InitializeComponent();
+            szwalnia_passed = szwalnia;
+            button1.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.TextLength > 0)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zapelnienie_regalu regal = new Zapelnienie_regalu(szwalnia_passed, textBox2.Text);
+            regal.Show();
+            this.Close();
         }
     }
 }
