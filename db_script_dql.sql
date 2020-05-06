f1e5b0e3cb15fc0e50322a6caccd6c74550fd26d
Lukasz Zarzycki <zrider100z@wp.pl>
2020-05-06 23:31:46 +0200

    More Views added

--- db_script_dql.sql
+++ db_script_dql.sql
@@ -1,5 +1,8 @@
-USE Szwalnia
+USE szwalnia
+---- Widok cech elementu
+
 GO
+
 CREATE VIEW [dbo].[vCechyElementu]
 AS
 SELECT Elementy.ID_Element,Cecha, Wartosc_Cechy_Liczbowa, Jednostka, Wartosc_Cechy_Slowna 
@@ -35,6 +38,16 @@ FROM            dbo.Polki INNER JOIN
                          dbo.Regaly ON dbo.Polki_regaly.ID_Regal = dbo.Regaly.ID_Regal
 GO
 
+CREATE VIEW vRealizacjaProcesuProdukcyjnegoDetails 
+AS
+SELECT dbo.Proces_Produkcyjny.ID_Procesu_Produkcyjnego, dbo.Realizacja_Procesu.ID_Realizacji_Procesu, dbo.Rodzaj_Etapu.Nazwa as 'Nazwa etapu', dbo.Realizacja_Procesu.Data_Rozpoczecia_Procesu, 
+                  dbo.Realizacja_Procesu.Data_Zakonczenia_Procesu
+FROM     dbo.Realizacja_Procesu INNER JOIN
+                  dbo.Rodzaj_Etapu ON dbo.Realizacja_Procesu.ID_Etapu = dbo.Rodzaj_Etapu.ID_Etapu INNER JOIN
+                  dbo.Proces_Produkcyjny ON dbo.Realizacja_Procesu.ID_Procesu_Produkcyjnego = dbo.Proces_Produkcyjny.ID_Procesu_Produkcyjnego
+				  
+				  
+GO
 
 -- widok Procesu Produkcyjnego dla ID Zamowienia
 CREATE VIEW vZamowienieProcesyProdukcyjne 
@@ -47,19 +60,6 @@ FROM            dbo.Proces_Produkcyjny INNER JOIN
                          dbo.Elementy_Typy ON dbo.Elementy.ID_Element_Typ = dbo.Elementy_Typy.ID_Element_Typ
 GO
 
-
-CREATE VIEW vRealizacjaProcesuProdukcyjnegoDetails 
-AS
-SELECT dbo.Proces_Produkcyjny.ID_Procesu_Produkcyjnego, dbo.Realizacja_Procesu.ID_Realizacji_Procesu, dbo.Rodzaj_Etapu.Nazwa as 'Nazwa etapu', dbo.Realizacja_Procesu.Data_Rozpoczecia_Procesu, 
-                  dbo.Realizacja_Procesu.Data_Zakonczenia_Procesu
-FROM     dbo.Realizacja_Procesu INNER JOIN
-                  dbo.Rodzaj_Etapu ON dbo.Realizacja_Procesu.ID_Etapu = dbo.Rodzaj_Etapu.ID_Etapu INNER JOIN
-                  dbo.Proces_Produkcyjny ON dbo.Realizacja_Procesu.ID_Procesu_Produkcyjnego = dbo.Proces_Produkcyjny.ID_Procesu_Produkcyjnego
-				  
-				  
-GO
-
-
 -- widok Procesu Produkcyjnego dla ID Zamowienie Element
 
 CREATE VIEW vIdZamowienieElementProcesProdukcyjny 
@@ -70,5 +70,58 @@ FROM            dbo.Proces_Produkcyjny INNER JOIN
                          dbo.Zamowienie_Element ON dbo.Proces_Produkcyjny.ID_Zamowienie_Element = dbo.Zamowienie_Element.ID_Zamowienie_Element
 GO
 
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------WIDOKI PRZYGOTOWANIE PRODUKCJI----------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
 
+CREATE VIEW vMaszyny_rodzaj_liczba AS
+SELECT TOP (100) PERCENT dbo.Maszyny_Proces.ID_Proces_Technologiczny, dbo.Rodzaj_Maszyny.Rodzaj_Maszyny, dbo.Srodki_Trwale.Producent, dbo.Maszyny_Proces.Liczba_Maszyn
+FROM     dbo.Rodzaj_Maszyny INNER JOIN
+                  dbo.Maszyny_Proces ON dbo.Rodzaj_Maszyny.ID_Rodzaj_Maszyny = dbo.Maszyny_Proces.ID_Rodzaj_Maszyny INNER JOIN
+                  dbo.Maszyny ON dbo.Rodzaj_Maszyny.ID_Rodzaj_Maszyny = dbo.Maszyny.ID_Rodzaj_Maszyny INNER JOIN
+                  dbo.Srodki_Trwale ON dbo.Maszyny.ID_Srodki_Trwale = dbo.Srodki_Trwale.ID_Srodki_trwale INNER JOIN
+                  dbo.Proces_Technologiczny ON dbo.Maszyny_Proces.ID_Proces_Technologiczny = dbo.Proces_Technologiczny.ID_Proces_Technologiczny INNER JOIN
+                  dbo.Proces_Zamowienie ON dbo.Proces_Technologiczny.ID_Proces_Technologiczny = dbo.Proces_Zamowienie.ID_Proces_Technologiczny INNER JOIN
+                  dbo.Etapy_W_Procesie ON dbo.Proces_Technologiczny.ID_Proces_Technologiczny = dbo.Etapy_W_Procesie.ID_Proces_Technologiczny INNER JOIN
+                  dbo.Rodzaj_Etapu ON dbo.Etapy_W_Procesie.ID_Etapu = dbo.Rodzaj_Etapu.ID_Etapu
+GROUP BY dbo.Maszyny_Proces.ID_Proces_Technologiczny, dbo.Rodzaj_Maszyny.Rodzaj_Maszyny, dbo.Srodki_Trwale.Producent, dbo.Maszyny_Proces.Liczba_Maszyn
+				  GO
 
+CREATE VIEW vMaszyny_serwis  AS
+SELECT dbo.Maszyny.ID_Maszyny, dbo.Maszyny.Resurs_Rbh, dbo.Maszyny.Serwis_Co_Ile, dbo.Srodki_Trwale.Producent, dbo.Srodki_Trwale.Nazwa, dbo.Srodki_Trwale.Numer_seryjny
+FROM     dbo.Maszyny INNER JOIN
+                  dbo.Srodki_Trwale ON dbo.Maszyny.ID_Srodki_Trwale = dbo.Srodki_Trwale.ID_Srodki_trwale
+				  GO
+
+CREATE VIEW vObsluga AS
+SELECT dbo.Obsluga_Techniczna.ID_Obsluga_Techniczna, dbo.Srodki_Trwale.Nazwa, dbo.Srodki_Trwale.Producent, dbo.Srodki_Trwale.Numer_seryjny, dbo.Rodzaj_Obslugi.Nazwa AS Expr1, dbo.Obsluga_Techniczna.Data_Wykonania, 
+                  dbo.Pracownicy.Nazwisko, dbo.Elementy.Element_Nazwa
+FROM     dbo.Rodzaj_Obslugi INNER JOIN
+                  dbo.Obsluga_Techniczna ON dbo.Rodzaj_Obslugi.ID_Rodzaj_Obslugi = dbo.Obsluga_Techniczna.ID_Rodzaj_Obslugi INNER JOIN
+                  dbo.Maszyny ON dbo.Obsluga_Techniczna.ID_Maszyny = dbo.Maszyny.ID_Maszyny INNER JOIN
+                  dbo.Srodki_Trwale ON dbo.Maszyny.ID_Srodki_Trwale = dbo.Srodki_Trwale.ID_Srodki_trwale INNER JOIN
+                  dbo.Pracownicy ON dbo.Obsluga_Techniczna.ID_Pracownika = dbo.Pracownicy.ID_Pracownika CROSS JOIN
+                  dbo.Elementy
+				  GO
+
+CREATE VIEW vSrednia_ilosc_maszyn AS
+SELECT dbo.Rodzaj_Maszyny.Rodzaj_Maszyny, SUM(dbo.Maszyny_Proces.Liczba_Maszyn) / COUNT(dbo.Proces_Zamowienie.ID_Proces_Zamowienie) AS srednia_ilosc_maszyn
+FROM     dbo.Proces_Zamowienie INNER JOIN
+                  dbo.Maszyny_Proces ON dbo.Maszyny_Proces.ID_Proces_Technologiczny = dbo.Proces_Zamowienie.ID_Proces_Technologiczny INNER JOIN
+                  dbo.Rodzaj_Maszyny ON dbo.Maszyny_Proces.ID_Rodzaj_Maszyny = dbo.Rodzaj_Maszyny.ID_Rodzaj_Maszyny
+GROUP BY dbo.Rodzaj_Maszyny.Rodzaj_Maszyny
+GO
+
+CREATE VIEW vSuma_czasu_procesu AS
+SELECT ID_Proces_Technologiczny, SUM(Czas) AS suma_czasu
+FROM     dbo.Etapy_W_Procesie
+GROUP BY ID_Proces_Technologiczny
+GO
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------KONIEC PRZYGOTOWANIE PRODUKCJI----------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------------------
+USE master
