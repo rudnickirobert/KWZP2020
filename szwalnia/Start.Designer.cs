f83104dfefe48500a56c847ad551ee1c1658550a
zuzannaroznowicz <zuzanna.roznowicz@student.wat.edu.pl>
2020-05-06 20:45:47 +0200

    Window positions changed.

--- szwalnia/Start.Designer.cs
+++ szwalnia/Start.Designer.cs
@@ -37,11 +37,11 @@
             // btnRealizacjaProcesu
             // 
             this.btnRealizacjaProcesu.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnRealizacjaProcesu.Location = new System.Drawing.Point(21, 132);
-            this.btnRealizacjaProcesu.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRealizacjaProcesu.Location = new System.Drawing.Point(479, 215);
+            this.btnRealizacjaProcesu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRealizacjaProcesu.Name = "btnRealizacjaProcesu";
             this.btnRealizacjaProcesu.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btnRealizacjaProcesu.Size = new System.Drawing.Size(160, 58);
+            this.btnRealizacjaProcesu.Size = new System.Drawing.Size(213, 71);
             this.btnRealizacjaProcesu.TabIndex = 1;
             this.btnRealizacjaProcesu.Text = "Realizacja procesu";
             this.btnRealizacjaProcesu.UseVisualStyleBackColor = true;
@@ -50,9 +50,10 @@
             // btnMagazyn
             // 
             this.btnMagazyn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnMagazyn.Location = new System.Drawing.Point(28, 34);
+            this.btnMagazyn.Location = new System.Drawing.Point(37, 42);
+            this.btnMagazyn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnMagazyn.Name = "btnMagazyn";
-            this.btnMagazyn.Size = new System.Drawing.Size(214, 75);
+            this.btnMagazyn.Size = new System.Drawing.Size(285, 92);
             this.btnMagazyn.TabIndex = 0;
             this.btnMagazyn.Text = "Magazyn";
             this.btnMagazyn.UseVisualStyleBackColor = true;
@@ -61,10 +62,10 @@
             // btnPrzydzialZasobow
             // 
             this.btnPrzydzialZasobow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnPrzydzialZasobow.Location = new System.Drawing.Point(377, 32);
-            this.btnPrzydzialZasobow.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPrzydzialZasobow.Location = new System.Drawing.Point(503, 39);
+            this.btnPrzydzialZasobow.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnPrzydzialZasobow.Name = "btnPrzydzialZasobow";
-            this.btnPrzydzialZasobow.Size = new System.Drawing.Size(142, 61);
+            this.btnPrzydzialZasobow.Size = new System.Drawing.Size(189, 75);
             this.btnPrzydzialZasobow.TabIndex = 1;
             this.btnPrzydzialZasobow.Text = "Przydzial Zasobow";
             this.btnPrzydzialZasobow.UseVisualStyleBackColor = true;
@@ -82,23 +83,24 @@
             // btnZarzadzanie
             // 
             this.btnZarzadzanie.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnZarzadzanie.Location = new System.Drawing.Point(21, 239);
-            this.btnZarzadzanie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnZarzadzanie.Location = new System.Drawing.Point(49, 205);
+            this.btnZarzadzanie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnZarzadzanie.Name = "btnZarzadzanie";
-            this.btnZarzadzanie.Size = new System.Drawing.Size(220, 72);
+            this.btnZarzadzanie.Size = new System.Drawing.Size(293, 89);
             this.btnZarzadzanie.TabIndex = 0;
             this.btnZarzadzanie.Text = "Zarzadzanie";
             this.btnZarzadzanie.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnZarzadzanie_MouseClick);
             // 
             // Start
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnZarzadzanie);
             this.Controls.Add(this.btnRealizacjaProcesu);
             this.Controls.Add(this.btnMagazyn);
             this.Controls.Add(this.btnPrzydzialZasobow);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Start";
             this.Text = "Szwalnia";
             this.ResumeLayout(false);
