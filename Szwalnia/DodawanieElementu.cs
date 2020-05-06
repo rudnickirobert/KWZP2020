6b19f761f2c5ddfbb274b8e95126f2523fc49707
PM142 <piotr.maleszyk@student.wat.edu.pl>
2020-05-07 00:07:49 +0200

    szablon

---
+++ Szwalnia/DodawanieElementu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szwalnia
+{
+    public partial class DodawanieElementu : Form
+    {
+        public DodawanieElementu()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvListaTypy_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            szwalnia.Oferta.Add(ofertaNew);
+            szwalnia.SaveChanges();
+        }
+    }
+}
