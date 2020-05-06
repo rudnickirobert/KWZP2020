4be1c58612c6d4fa9b8e3c1f23c9e6b2510c13a6
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-06 23:39:01 +0200

    Dodany i zaprojektowany wstępnie formularz UkladMagazynu

---
+++ Szwalnia/UkladMagazynu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szwalnia
+{
+    public partial class UkladMagazynu : Form
+    {
+        public UkladMagazynu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZnajdzPolke_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDodajRegal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDodajPolke_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDodajRozmiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
