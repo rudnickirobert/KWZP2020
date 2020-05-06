de65b59549cbda8bdbeb1417b018c4c29e164e4b
zuzannaroznowicz <zuzanna.roznowicz@student.wat.edu.pl>
2020-05-06 20:45:26 +0200

    Database updated to current state.

--- Szwalnia/Start.cs
+++ Szwalnia/Start.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Szwalnia
 {
@@ -26,20 +34,5 @@ namespace Szwalnia
             ZarzadzanieForm zarzadzanieForm = new ZarzadzanieForm(szwalnia);
             zarzadzanieForm.Show();
         }
-
-        private void btnPrzygotowanieProdukcji_Click(object sender, EventArgs e)
-        {
-            PrzygotowanieProdukcji przygotowanieProdukcji = new PrzygotowanieProdukcji(szwalnia);
-            przygotowanieProdukcji.Show();
-        }
-
-
-    
-
-        private void btnProdukcja_Click(object sender, EventArgs e)
-        {
-            Produkcja formularzProdukcji = new Produkcja(szwalnia);
-            formularzProdukcji.Show();
-        }
-    } 
+    }
 }
