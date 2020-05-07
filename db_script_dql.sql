8abb7001f0223e27231475cca900b3b6a1a0c7cb
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-07 17:18:34 +0200

    Zmienione pliki bazy danych (typy danych w Polki_Rozmiary)

--- db_script_dql.sql
+++ db_script_dql.sql
@@ -27,7 +27,8 @@ GO
 ---- Widok rozmiarow polek (posegregowane po wymiarach)
 CREATE VIEW [dbo].[vPolki_Rozmiary]
 AS
-SELECT        TOP (100) PERCENT ID_Rozmiar_Polki, Wysokosc, Szerokosc, Glebokosc, Wysokosc + ' x ' + Szerokosc + ' x ' + Glebokosc + ' cm' AS Wymiar
+SELECT        TOP (100) PERCENT ID_Rozmiar_Polki, Wysokosc, Szerokosc, Glebokosc, 
+				CAST(Wysokosc AS NVARCHAR) + ' x ' + CAST(Szerokosc AS NVARCHAR) + ' x ' + CAST(Glebokosc AS NVARCHAR) + ' cm' AS Wymiar
 FROM            dbo.Polki_Rozmiary
 ORDER BY Wysokosc, Szerokosc, Glebokosc
 GO
