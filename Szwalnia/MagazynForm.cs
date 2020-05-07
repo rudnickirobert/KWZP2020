e3554a080ce731792f08d0b15f0c8337e04145c4
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-07 19:22:09 +0200

    Dodane ikony formularzy. Dodane przyciski na MagazynForm, utworzone kilka następnych formularzy magazynowych.

--- Szwalnia/MagazynForm.cs
+++ Szwalnia/MagazynForm.cs
@@ -12,24 +12,38 @@ namespace Szwalnia
 {
     public partial class MagazynForm : Form
     {
-        public SzwalniaEntities szwalniaPassed;
+        public SzwalniaEntities db;
         public MagazynForm(SzwalniaEntities szwalnia)
         {
-            szwalniaPassed = szwalnia;
+            db = szwalnia;
             InitializeComponent();
         }
 
         private void btnElementyForm_Click(object sender, EventArgs e)
         {
-            WyborElementu wybor = new WyborElementu(szwalniaPassed);
+            WyborElementu wybor = new WyborElementu(db);
             wybor.Show();
             this.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            UkladMagazynu uklad = new UkladMagazynu(szwalniaPassed);
+            UkladMagazynu uklad = new UkladMagazynu(db);
             uklad.Show();
+            this.Close();
+        }
+
+        private void btnStanMagazynowy_Click(object sender, EventArgs e)
+        {
+            StanMagazynowy magazynowy = new StanMagazynowy(db);
+            magazynowy.Show();
+            this.Close();
+        }
+
+        private void btnObslugaZamowien_Click(object sender, EventArgs e)
+        {
+            ObslugaZamowien zamowienia = new ObslugaZamowien(db);
+            zamowienia.Show();
             this.Close();
         }
     }
