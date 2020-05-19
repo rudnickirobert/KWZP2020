95d4ab85e0c566f5efd0a3772a7fd65cf1cd05eb
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-19 14:03:03 +0200

    Drobne poprawki w formularzach stanu magazynowego

--- Szwalnia/StanMagazynowyElementy.cs
+++ Szwalnia/StanMagazynowyElementy.cs
@@ -18,7 +18,6 @@ namespace Szwalnia
             InitializeComponent();
             db = Start.szwalnia;
             dgvStanMagazynowyElementy.DataSource = db.vStan_magazynowy_elementy.ToList();
-            dgvStanMagazynowyElementy.Columns[0].Visible = false;
         }
 
         private void dgvStanMagazynowyElementy_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
