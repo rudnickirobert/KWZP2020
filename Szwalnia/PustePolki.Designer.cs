a4984844e49e1c9e146d2a3f3c22a7e2b7daf6f6
PM142 <piotr.maleszyk@student.wat.edu.pl>
2020-05-19 21:19:07 +0200

    Revert "Drobne poprawki w formularzach stanu magazynowego"
    This reverts commit 95d4ab85e0c566f5efd0a3772a7fd65cf1cd05eb.

--- Szwalnia/PustePolki.Designer.cs
+++ Szwalnia/PustePolki.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PustePolki));
             this.dgvPustePolki = new System.Windows.Forms.DataGridView();
             this.lblPustePolki = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPustePolki)).BeginInit();
@@ -41,14 +40,14 @@
             this.dgvPustePolki.Location = new System.Drawing.Point(19, 61);
             this.dgvPustePolki.MultiSelect = false;
             this.dgvPustePolki.Name = "dgvPustePolki";
-            this.dgvPustePolki.Size = new System.Drawing.Size(363, 440);
+            this.dgvPustePolki.Size = new System.Drawing.Size(341, 440);
             this.dgvPustePolki.TabIndex = 13;
             // 
             // lblPustePolki
             // 
             this.lblPustePolki.AutoSize = true;
             this.lblPustePolki.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
-            this.lblPustePolki.Location = new System.Drawing.Point(94, 17);
+            this.lblPustePolki.Location = new System.Drawing.Point(77, 18);
             this.lblPustePolki.Name = "lblPustePolki";
             this.lblPustePolki.Size = new System.Drawing.Size(162, 31);
             this.lblPustePolki.TabIndex = 14;
@@ -58,12 +57,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 516);
+            this.ClientSize = new System.Drawing.Size(372, 516);
             this.Controls.Add(this.dgvPustePolki);
             this.Controls.Add(this.lblPustePolki);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PustePolki";
-            this.Text = "Puste półki";
+            this.Text = "PustePolki";
             ((System.ComponentModel.ISupportInitialize)(this.dgvPustePolki)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
