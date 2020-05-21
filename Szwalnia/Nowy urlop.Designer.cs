fb9e7b5afa05eef024dcaf600493e01f8d6e9773
zuzannaroznowicz <zuzanna.roznowicz@student.wat.edu.pl>
2020-05-21 15:26:59 +0200

    dodannie urlopow

---
+++ Szwalnia/Nowy urlop.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Szwalnia
+{
+    partial class NowyUrlop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPracownik = new System.Windows.Forms.ComboBox();
+            this.cbRodzaj = new System.Windows.Forms.ComboBox();
+            this.dtpRozpoczecie = new System.Windows.Forms.DateTimePicker();
+            this.dtpZakonczenie = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnZamknij = new System.Windows.Forms.Button();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnAnuluj = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbPracownik
+            // 
+            this.cbPracownik.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbPracownik.FormattingEnabled = true;
+            this.cbPracownik.Location = new System.Drawing.Point(206, 80);
+            this.cbPracownik.Name = "cbPracownik";
+            this.cbPracownik.Size = new System.Drawing.Size(337, 33);
+            this.cbPracownik.TabIndex = 0;
+            // 
+            // cbRodzaj
+            // 
+            this.cbRodzaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbRodzaj.FormattingEnabled = true;
+            this.cbRodzaj.Location = new System.Drawing.Point(206, 131);
+            this.cbRodzaj.Name = "cbRodzaj";
+            this.cbRodzaj.Size = new System.Drawing.Size(337, 33);
+            this.cbRodzaj.TabIndex = 1;
+            // 
+            // dtpRozpoczecie
+            // 
+            this.dtpRozpoczecie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dtpRozpoczecie.Location = new System.Drawing.Point(206, 184);
+            this.dtpRozpoczecie.Name = "dtpRozpoczecie";
+            this.dtpRozpoczecie.Size = new System.Drawing.Size(337, 30);
+            this.dtpRozpoczecie.TabIndex = 3;
+            // 
+            // dtpZakonczenie
+            // 
+            this.dtpZakonczenie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dtpZakonczenie.Location = new System.Drawing.Point(206, 235);
+            this.dtpZakonczenie.Name = "dtpZakonczenie";
+            this.dtpZakonczenie.Size = new System.Drawing.Size(337, 30);
+            this.dtpZakonczenie.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(21, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pracownik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(21, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rodzaj urlopu";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(21, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Data rozpoczęcia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(21, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(168, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Data zakończenia";
+            // 
+            // btnZamknij
+            // 
+            this.btnZamknij.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnZamknij.Location = new System.Drawing.Point(404, 310);
+            this.btnZamknij.Name = "btnZamknij";
+            this.btnZamknij.Size = new System.Drawing.Size(139, 63);
+            this.btnZamknij.TabIndex = 10;
+            this.btnZamknij.Text = "Zapisz i zamknij";
+            this.btnZamknij.UseVisualStyleBackColor = true;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDodaj.Location = new System.Drawing.Point(184, 310);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(168, 63);
+            this.btnDodaj.TabIndex = 11;
+            this.btnDodaj.Text = "Dodaj kolejny";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // btnAnuluj
+            // 
+            this.btnAnuluj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnAnuluj.Location = new System.Drawing.Point(24, 310);
+            this.btnAnuluj.Name = "btnAnuluj";
+            this.btnAnuluj.Size = new System.Drawing.Size(99, 63);
+            this.btnAnuluj.TabIndex = 12;
+            this.btnAnuluj.Text = "Anuluj";
+            this.btnAnuluj.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(153, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 32);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Dodaj nowy urlop";
+            // 
+            // NowyUrlop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(564, 403);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAnuluj);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.btnZamknij);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpZakonczenie);
+            this.Controls.Add(this.dtpRozpoczecie);
+            this.Controls.Add(this.cbRodzaj);
+            this.Controls.Add(this.cbPracownik);
+            this.Name = "NowyUrlop";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nowy urlop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPracownik;
+        private System.Windows.Forms.ComboBox cbRodzaj;
+        private System.Windows.Forms.DateTimePicker dtpRozpoczecie;
+        private System.Windows.Forms.DateTimePicker dtpZakonczenie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnZamknij;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnAnuluj;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Szwalnia/Nowy urlop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szwalnia
+{
+    public partial class NowyUrlop : Form
+    {
+        public SzwalniaEntities db;
+        public NowyUrlop(SzwalniaEntities db)
+        {
+            InitializeComponent();
+            this.db = db;
+
+            cbPracownik.DataSource = db.vImieNazwiskoPracownika.ToList();
+            cbPracownik.ValueMember = "ID_Pracownika";
+            cbPracownik.DisplayMember = "Imie i nazwisko pracownika";
+
+            cbRodzaj.DataSource = db.Rodzaj_Urlopu.ToList();
+            cbRodzaj.ValueMember = "ID_Rodzaj_Urlopu";
+            cbRodzaj.DisplayMember = "Nazwa";
+            cbRodzaj.Refresh();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            Urlop urlop = new Urlop();
+            urlop.ID_Pracownika = Convert.ToInt32(cbPracownik.SelectedValue);
+            urlop.ID_Rodzaj_Urlopu = Convert.ToInt32(cbRodzaj.SelectedValue);
+            urlop.Data_rozpoczecia = Convert.ToDateTime(dtpRozpoczecie.Value);
+            urlop.Data_zakonczenia = Convert.ToDateTime(dtpZakonczenie.Value);
+            db.Urlop.Add(urlop);
+            db.SaveChanges();
+            MessageBox.Show("Dodano nowe urlop pracownikowi");
+            this.Close();
+        }
+    }
+}
