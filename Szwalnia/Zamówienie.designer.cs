885fda864c04508ebb8d22ce07050c3bb6a84077
zuzannaroznowicz <zuzanna.roznowicz@student.wat.edu.pl>
2020-05-21 10:17:50 +0200

    Dodanie do panelu zamówień przycisków otwierających formularze z produkcji mówiące o czasie wykonania produktu

--- Szwalnia/Zamówienie.designer.cs
+++ Szwalnia/Zamówienie.designer.cs
@@ -33,14 +33,16 @@
             this.btnKoniecZamowienia = new System.Windows.Forms.Button();
             this.btnZamknij = new System.Windows.Forms.Button();
             this.btnDodajElementy = new System.Windows.Forms.Button();
+            this.btnCzasTyp = new System.Windows.Forms.Button();
+            this.btnCzasTechnologia = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStare
             // 
             this.btnStare.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnStare.Location = new System.Drawing.Point(23, 26);
+            this.btnStare.Location = new System.Drawing.Point(41, 36);
             this.btnStare.Name = "btnStare";
-            this.btnStare.Size = new System.Drawing.Size(215, 109);
+            this.btnStare.Size = new System.Drawing.Size(470, 109);
             this.btnStare.TabIndex = 0;
             this.btnStare.Text = "Podgląd zamówień";
             this.btnStare.UseVisualStyleBackColor = true;
@@ -49,9 +51,9 @@
             // btnNowe
             // 
             this.btnNowe.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnNowe.Location = new System.Drawing.Point(281, 26);
+            this.btnNowe.Location = new System.Drawing.Point(552, 36);
             this.btnNowe.Name = "btnNowe";
-            this.btnNowe.Size = new System.Drawing.Size(215, 109);
+            this.btnNowe.Size = new System.Drawing.Size(470, 109);
             this.btnNowe.TabIndex = 6;
             this.btnNowe.Text = "Nowe Zamówienie";
             this.btnNowe.UseVisualStyleBackColor = true;
@@ -60,9 +62,9 @@
             // btnKoniecZamowienia
             // 
             this.btnKoniecZamowienia.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnKoniecZamowienia.Location = new System.Drawing.Point(281, 198);
+            this.btnKoniecZamowienia.Location = new System.Drawing.Point(552, 292);
             this.btnKoniecZamowienia.Name = "btnKoniecZamowienia";
-            this.btnKoniecZamowienia.Size = new System.Drawing.Size(215, 109);
+            this.btnKoniecZamowienia.Size = new System.Drawing.Size(470, 109);
             this.btnKoniecZamowienia.TabIndex = 7;
             this.btnKoniecZamowienia.Text = "Zakończ zamówienie";
             this.btnKoniecZamowienia.UseVisualStyleBackColor = true;
@@ -70,7 +72,7 @@
             // 
             // btnZamknij
             // 
-            this.btnZamknij.Location = new System.Drawing.Point(378, 381);
+            this.btnZamknij.Location = new System.Drawing.Point(904, 419);
             this.btnZamknij.Name = "btnZamknij";
             this.btnZamknij.Size = new System.Drawing.Size(118, 54);
             this.btnZamknij.TabIndex = 8;
@@ -81,20 +83,44 @@
             // btnDodajElementy
             // 
             this.btnDodajElementy.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnDodajElementy.Location = new System.Drawing.Point(23, 198);
+            this.btnDodajElementy.Location = new System.Drawing.Point(552, 163);
             this.btnDodajElementy.Name = "btnDodajElementy";
-            this.btnDodajElementy.Size = new System.Drawing.Size(215, 109);
+            this.btnDodajElementy.Size = new System.Drawing.Size(470, 109);
             this.btnDodajElementy.TabIndex = 9;
             this.btnDodajElementy.Text = "Dodaj elementy do istniejącego zamówienia";
             this.btnDodajElementy.UseVisualStyleBackColor = true;
             this.btnDodajElementy.Click += new System.EventHandler(this.btnDodajElementy_Click);
+            // 
+            // btnCzasTyp
+            // 
+            this.btnCzasTyp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnCzasTyp.Location = new System.Drawing.Point(41, 163);
+            this.btnCzasTyp.Name = "btnCzasTyp";
+            this.btnCzasTyp.Size = new System.Drawing.Size(470, 109);
+            this.btnCzasTyp.TabIndex = 10;
+            this.btnCzasTyp.Text = "Szacowany czas wykonania elementu według typu";
+            this.btnCzasTyp.UseVisualStyleBackColor = true;
+            this.btnCzasTyp.Click += new System.EventHandler(this.btnCzasTyp_Click);
+            // 
+            // btnCzasTechnologia
+            // 
+            this.btnCzasTechnologia.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnCzasTechnologia.Location = new System.Drawing.Point(41, 292);
+            this.btnCzasTechnologia.Name = "btnCzasTechnologia";
+            this.btnCzasTechnologia.Size = new System.Drawing.Size(470, 109);
+            this.btnCzasTechnologia.TabIndex = 11;
+            this.btnCzasTechnologia.Text = "Szacowany czas wykonania elementu według technologii";
+            this.btnCzasTechnologia.UseVisualStyleBackColor = true;
+            this.btnCzasTechnologia.Click += new System.EventHandler(this.btnCzasTechnologia_Click);
             // 
             // Zamowienie
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.ClientSize = new System.Drawing.Size(517, 462);
+            this.ClientSize = new System.Drawing.Size(1080, 495);
+            this.Controls.Add(this.btnCzasTechnologia);
+            this.Controls.Add(this.btnCzasTyp);
             this.Controls.Add(this.btnDodajElementy);
             this.Controls.Add(this.btnZamknij);
             this.Controls.Add(this.btnKoniecZamowienia);
@@ -114,5 +140,7 @@
         private System.Windows.Forms.Button btnKoniecZamowienia;
         private System.Windows.Forms.Button btnZamknij;
         private System.Windows.Forms.Button btnDodajElementy;
+        private System.Windows.Forms.Button btnCzasTyp;
+        private System.Windows.Forms.Button btnCzasTechnologia;
     }
 }
